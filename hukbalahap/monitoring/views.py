96262a340756f0eccf1b9a5a2d7a7ac6a722404f
PengBanana <thealvinsep@gmail.com>
2018-06-30 14:44:40 +0800

    views.py edit form ready

---
+++ hukbalahap/monitoring/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Pool, Usertype_Ref, User, MaintenanceSchedule, Temp_Turbidity, Temp_Temperature, Temp_Ph, Final_Turbidity, Final_Temperature, Final_Ph
+def index(request):
+    pool = Pool.objects.all()
+    temperature = Temp_Temperature.objects.all()
+    turbidity = Temp_Turbidity.objects.all()
+    ph = Temp_Ph.objects.all()
+    content= {
+        'pool':pool,
+        'temperature':temperature,
+        'turbidity':turbidity,
+        'ph':ph,
+    }
+    return render(request, 'monitoring/pool technician/home.html', content)
+
+def login(request):
+    return render(request, 'monitoring/login.html')
+
+def pool(request):
+    return render(request, 'monitoring/pool technician/pool-stat.html')
+
+def indexOwner(request):
+    return render(request, 'monitoring/pool owner/home-owner.html')
+def firstLogin(request):
+    return render(request, 'monitoring/pool technician/first-login.html')
+def addUser(request):
+    return render(request, 'monitoring/pool owner/add-user.html')
+def personnel(request):
+    return render(request, 'monitoring/pool owner/personnel-efficiency.html')
+def search(request):
+    return render(request, 'monitoring/pool owner/search-technician.html')
+def profile(request):
+    return render(request, 'monitoring/pool owner/technician-profile.html')

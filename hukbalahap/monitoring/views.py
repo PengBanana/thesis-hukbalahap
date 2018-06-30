0c6f164e133adbd71a326f475c3b1f00ba6e0fc2
PengBanana <thealvinsep@gmail.com>
2018-06-30 17:30:30 +0800

    standard deviation of pool temperature

--- hukbalahap/monitoring/views.py
+++ hukbalahap/monitoring/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Pool, Usertype_Ref, User, MaintenanceSchedule, Temp_Turbidity, Temp_Temperature, Temp_Ph, Final_Turbidity, Final_Temperature, Final_Ph
 from monitoring.forms import AddUserForm
 from django.views.generic import TemplateView
-
+from django.db.models import Sum, Count
+import math
 
 
 class AddUserView(TemplateView):
@@ -15,11 +16,33 @@ class AddUserView(TemplateView):
 
 
 def index(request):
-    pool = Pool.objects.all()
+    poolref = Pool.objects.all()
     temperature = Temp_Temperature.objects.all()
     turbidity = Temp_Turbidity.objects.all()
     ph = Temp_Ph.objects.all()
+    
+    poolCount = Pool.objects.all().count()
+    tempdeviations = []
+    
+    #standard deviation of one pool
+    itemTemperature = Temp_Temperature.objects.filter(pool=poolref.get(pk=1))
+    lvltemp = itemTemperature.annotate(temperaturesum=Sum('temp_temperaturelevel'))
+    lvltempCount = itemTemperature.annotate(temperatureCount=Count('temp_temperaturelevel'))
+    tempSum=lvltemp.get().temperaturesum
+    tempCount=lvltempCount.get().temperatureCount
+    tempMean = tempSum/tempCount
+    tempx = []
+    for level in itemTemperature:
+        reading = level.temp_temperaturelevel
+        reading -=tempMean
+        tempx.append(reading)
+    newTempSum = 0
+    for read in tempx:
+        newTempSum+= read
+    tempVariance = newTempSum/tempCount
+    tempStandardDev = math.sqrt(tempVariance )
     content= {
+        'poolCount': newTempSum,
         'pool':pool,
         'temperature':temperature,
         'turbidity':turbidity,

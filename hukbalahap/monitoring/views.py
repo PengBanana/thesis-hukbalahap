e8d349ad1087f129cf52d4a78815d205a108f8fe
PengBanana <thealvinsep@gmail.com>
2018-08-13 04:19:27 +0800

    asdh

--- hukbalahap/monitoring/views.py
+++ hukbalahap/monitoring/views.py
@@ -133,22 +133,20 @@ def batchCount10pH():
         pHStandardDev= decimal.Decimal(pHStandardDev)+tempMean
         pHStandardDev = round(pHStandardDev, 1)
         ##new notification
-        try:
-            if pHStandardDev < 7.2 or pHStandardDev > 7.8:
-                poolx=Pool.objects.get(id=1)
-                userx = User.objects.get(Username="pooltech3")
-                messagex=poolx.pool_location+" needs attention"
-                try:
-                    getNotification=Notification_Table.objects.get(user=userx, number=1)
-                except Notification_Table.DoesNotExist:
-                    newNotification= Notification_Table(
-                        user=userx,
-                        message=messagex,
-                        number = 1
-                    )
-                    newNotification.save()
-        except:
-            print("yah")
+        if pHStandardDev < 7.2 or pHStandardDev > 7.8:
+            poolx=Pool.objects.get(id=1)
+            userx = User.objects.get(Username="pooltech3")
+            messagex=poolx.pool_location+" needs attention"
+            try:
+                getNotification=Notification_Table.objects.get(user=userx, number=1)
+            except Notification_Table.DoesNotExist:
+                newNotification= Notification_Table(
+                    user=userx,
+                    message=messagex,
+                    number = 1
+                )
+                newNotification.save()
+        print("yah")
         ##end of new notification
         Final_Ph.objects.create(pool_id='1', final_phlevel=pHStandardDev, final_phdatetime=datetime.datetime.now())
         print("Final_Ph Value Added: Enrique Razon Building, " + str(pHStandardDev) + ", " + str(datetime.datetime.now()))

11414c58df24d35aba690d66c87b01a2c06ffc00
PengBanana <thealvinsep@gmail.com>
2018-08-14 04:07:11 +0800

    viewMaintenance debugging mode

--- hukbalahap/monitoring/views.py
+++ hukbalahap/monitoring/views.py
@@ -1273,7 +1273,7 @@ def personnel(request):
 @login_required(login_url="/monitoring/login")
 def maintenanceDetails(request, schedule_id):
     notifications = getNotification(request)
-    try:
+    if 0==0:
         actual=0
         item = MaintenanceSchedule.objects.get(id=schedule_id)
         pool = item.pool
@@ -1416,7 +1416,7 @@ def maintenanceDetails(request, schedule_id):
         }
         #insert notification here content.append/content.add(function())
         return render(request, 'monitoring/pool technician/maintenance-details.html', content)
-    except:
+    else:
         content={
             "backUrl":"monitoring:viewMaintenance"
         }

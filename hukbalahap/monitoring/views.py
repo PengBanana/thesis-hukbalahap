9084f0ee8927b0085e88e2f70aec31482c6a070b
Alvin <thealvinsep@gmail.com>
2018-10-10 19:01:05 +0800

    db reset

--- hukbalahap/monitoring/views.py
+++ hukbalahap/monitoring/views.py
@@ -21,7 +21,6 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
         userx = authenticate(username=username, password=password)
-
         if userx is not None:
             userStat =Status.objects.get(id=userx.pk)
             notDeactivated =  Status_Ref.objects.get(pk=1)

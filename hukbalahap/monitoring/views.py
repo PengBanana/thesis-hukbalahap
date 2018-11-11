6015941bc79206acdf16f03f48973e31cb7ad52c
PengBananaKraken <thealvinsep@gmail.com>
2018-11-12 03:04:05 +0800

    employe Performance Report Bug Fix

--- hukbalahap/monitoring/views.py
+++ hukbalahap/monitoring/views.py
@@ -1780,7 +1780,8 @@ def personnelEfficiency(request):
     efficiencyList=[]
     eNames=[]
     for employee in employeeList:
-        if employee.type == poolTechType:
+        usertype = Type.objects.get(user=employee)
+        if usertype.type == poolTechType:
             eName=str(employee.first_name)+" "+str(employee.last_name)
             eNames.append(eName)
             employeeReport=MaintenanceSchedule.objects.all().filter(user=employee)
@@ -1811,6 +1812,7 @@ def personnelEfficiency(request):
         averageAverage=round(averageAverage)
     except:
         averageAverage=0
+    print(eNames)
     content={
         'dd':displayDate,
         "zl":eNames,
